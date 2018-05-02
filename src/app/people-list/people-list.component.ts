1166c899e2702d27bea9eecbe76e5a88b53f42cf
Daniel English <denglish7@gmail.com>
2018-05-02 12:50:10 -0700

    initial buildout

---
+++ src/app/people-list/people-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Person } from '../person';
+import { PeopleService } from '../services/people.service';
+
+@Component({
+  selector: 'app-people-list',
+  template: `
+    <ul>
+      <li *ngFor="let person of people">
+        <a [routerLink]="['/persons', person.id]">
+          {{person.name}}
+        </a>
+      </li>
+    </ul>
+  `,
+  styleUrls: ['./people-list.component.scss']
+})
+
+export class PeopleListComponent implements OnInit {
+  people: Person[] = [];
+
+
+  constructor(private _peopleService: PeopleService) { }
+
+  ngOnInit() {
+    this._peopleService
+        .getAll()
+        .subscribe(p => this.people = p);
+  }
+}
